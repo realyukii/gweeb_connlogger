6d7a221cedc9018e153a6988269da7e59c43e0ff
Real Yuki <me@reyuki.site>
2025-05-20 19:49:01 +0700

    test patched connect function

--- connlogger.c
+++ connlogger.c
@@ -1,9 +1,25 @@
-void external_func(void) {
-    int a = 10, b;
-    asm volatile("movl %1, %%eax;"
-        "movl %%eax, %0;"
-        :"=r" (b)
-        :"r" (a)
-        :"%eax"
-        );
+#include <arpa/inet.h>
+#include <errno.h>
+#include <stdio.h>
+
+int connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen) {
+	int ret;
+	asm volatile(
+			"syscall"
+		:	"=a" (ret)
+		:	"a" (42),		/* %rax */
+			"D" (sockfd)	/* %rdi */
+			"S" (addr)		/* %rsi */
+			"d" (addrlen)	/* %rdx */
+	);
+
+	if (ret < 0) {
+		puts("failed");
+		errno = -ret;
+		return -1;
+	}
+
+	puts("success");
+
+	return 0;
 }
